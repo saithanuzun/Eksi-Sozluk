cdd7ba09e3cb5624ba387cd16288825a9ea0c1b5
sait han uzun <saithan.uzun@gmail.com>
2023-03-23 17:26:12 +0000

    Add DbContext

---
+++ src/Api/Infrastructure/EksiSozluk.Api.Infrastructure.Persistence/Context/EksiSozlukContext.cs
@@ -0,0 +1,73 @@
+using System.Reflection;
+using EksiSozluk.Api.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EksiSozluk.Api.Infrastructure.Persistence.Context;
+
+public class EksiSozlukContext : DbContext
+{
+    public const string DEFAULT_SCHEMA = "dbo";
+
+    public EksiSozlukContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+
+    public DbSet<Entry> Entries { get; set; }
+    public DbSet<EntryVote> EntryVotes { get; set; }
+    public DbSet<EntryFavorite> EntryFavorites { get; set; }
+
+    public DbSet<EntryComment> EntryComments { get; set; }
+    public DbSet<EntryCommentVote> EntryCommentVotes { get; set; }
+    public DbSet<EntryCommentFavorite> EntryCommentFavorites { get; set; }
+
+    public DbSet<EmailConfirmation> EmailConfirmations { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+
+    public override int SaveChanges(bool acceptAllChangesOnSuccess)
+    {
+        OnBeforeSave();
+        return base.SaveChanges(acceptAllChangesOnSuccess);
+    }
+
+    public override int SaveChanges()
+    {
+        OnBeforeSave();
+        return base.SaveChanges();
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        OnBeforeSave();
+        return base.SaveChangesAsync(cancellationToken);
+    }
+
+    public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess,
+        CancellationToken cancellationToken = new CancellationToken())
+    {
+        OnBeforeSave();
+        return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+    }
+
+    private void OnBeforeSave()
+    {
+        var AddedEntities = ChangeTracker.Entries()
+            .Where(i => i.State == EntityState.Added)
+            .Select(i => (BaseEntity)i.Entity);
+        PrepareAddedEntity(AddedEntities);
+    }
+
+    private void PrepareAddedEntity(IEnumerable<BaseEntity> entities)
+    {
+        foreach (var entity in entities)
+        {
+            if (entity.CreatedDate == DateTime.MinValue)
+                entity.CreatedDate = DateTime.Now;
+        }
+    }
+}
