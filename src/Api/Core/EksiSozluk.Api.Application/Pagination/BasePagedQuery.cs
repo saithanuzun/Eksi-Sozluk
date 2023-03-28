27a5ee631a70d6c1537ff7c9984c4f45ef30e962
sait han uzun <saithan.uzun@gmail.com>
2023-03-28 03:20:13 +0100

    Add pagination

---
+++ src/Api/Core/EksiSozluk.Api.Application/Pagination/BasePagedQuery.cs
@@ -0,0 +1,14 @@
+namespace EksiSozluk.Api.Application.Pagination;
+
+public class BasePagedQuery
+{
+    public BasePagedQuery(int page, int pageSize)
+    {
+        Page = page;
+        PageSize = pageSize;
+    }
+
+    public int Page { get; set; }
+
+    public int PageSize { get; set; }
+}
