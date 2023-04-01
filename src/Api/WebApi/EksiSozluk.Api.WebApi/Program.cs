bb9442b11d8858b9319dfa58ab8cd8b296c9ac79
sait han uzun <saithan.uzun@gmail.com>
2023-04-01 23:22:12 +0100

    Add WebApp CSS Files

--- src/Api/WebApi/EksiSozluk.Api.WebApi/Program.cs
+++ src/Api/WebApi/EksiSozluk.Api.WebApi/Program.cs
@@ -19,6 +19,12 @@ builder.Services.AddSwaggerGen();
 builder.Services.ConfigureAuth(builder.Configuration);
 builder.Services.AddInfrastructureRegistration(builder.Configuration);
 builder.Services.AddApplicationRegistration();
+builder.Services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+{
+    builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+}));
 
 var app = builder.Build();
 
@@ -34,6 +40,8 @@ app.UseHttpsRedirection();
 app.ConfigureExceptionHandling(app.Environment.IsDevelopment());
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseCors("MyPolicy");
 
 app.MapControllers();
 
