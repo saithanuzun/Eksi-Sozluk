e17ea8a490877229602dd71b9dd71bc8bec8c286
sait han uzun <saithan.uzun@gmail.com>
2023-04-08 23:43:35 +0100

    Add BlazorApp RateComponents

---
+++ src/Clients/EksiSozluk.BlazorApp/Infrastructure/Models/EventArgs/FavClickedEventArgs.cs
@@ -0,0 +1,7 @@
+﻿
+public class FavClickedEventArgs: EventArgs
+{
+    public Guid? EntryId { get; set; }
+
+    public bool IsFaved { get; set; }
+}
---
+++ src/Clients/EksiSozluk.BlazorApp/Infrastructure/Models/EventArgs/VoteClickedEventArgs.cs
@@ -0,0 +1,13 @@
+﻿
+public class VoteClickedEventArgs: EventArgs
+{
+    public Guid EntryId { get; set; }
+
+    public bool IsUpVoteClicked { get; set; }
+
+    public bool UpVoteDeleted { get; set; }
+
+
+    public bool IsDownVoteClicked { get; set; }
+    public bool DownVoteDeleted { get; set; }
+}
